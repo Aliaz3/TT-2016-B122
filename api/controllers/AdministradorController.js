c9ec805a3c5377e4e9d9a88473ff88bad7ad3e20
Alan Díaz <mk.dir35@gmail.com>
2017-10-14 19:47:58 -0500

    ultimo comitt por ahora
    flash error

--- api/controllers/AdministradorController.js
+++ api/controllers/AdministradorController.js
@@ -8,13 +8,22 @@
 module.exports = {
 	
 	'new': function (req, res){
+		res.locals.flash = _.clone(req.session.flash);
 		res.view();
+		req.session.flash ={};
 	},
 	
 	create : function (req, res, next){
 		Administrador.create( req.params.all(), function userCreated(err, user){
-			if(err) return next(err);
+			if(err) {
+				console.log(err);
+				req.session.flash={
+					err:err
+				}
+				return res.redirect('/administrador/new');
+			}
 			res.json(user);
+			req.session.flash ={};
 		});
 	}
 	
