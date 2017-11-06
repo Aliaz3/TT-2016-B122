f6ea3a28b061ac9d066c6d458a514ee6bd8d1ffd
Alan Díaz <mk.dir35@gmail.com>
2017-11-06 05:45:14 -0600

    escribir pdfs
    esta bien del diablo D:

--- api/controllers/TrabajoSocialController.js
+++ api/controllers/TrabajoSocialController.js
@@ -28,8 +28,69 @@ module.exports = {
 		var ojb = req.allParams();
 		console.log(ojb);
 	},
-	act130 : function (req, res, next){
-		
+	act130 : function (req, res){
+		var date = new Date();
+		console.log(date);
+		console.log('1');
+		 var year = date.getFullYear();
+
+		var month = date.getMonth() + 1;
+		month = (month < 10 ? "0" : "") + month;
+
+		var day  = date.getDate();
+		day = (day < 10 ? "0" : "") + day;
+		var ojb = req.allParams();
+		console.log(ojb);
+				//res.writeHead(200, {'Content-Type': 'application/pdf'});
+				console.log('2');
+				var hummus = require('hummus');
+				console.log('3');
+				var pdfWriter = hummus.createWriterToModify('C:\\Users\\Alan\\Documents\\TT-2016-B122\\FACS\\TRABAJO SOCIAL\\FAC130.PDF',{modifiedFilePath:'C:\\Users\\Alan\\Documents\\TT-2016-B122\\FACS\\TRABAJO SOCIAL\\FAC1302.PDF'});
+				console.log('4');
+				var pageModifier = new hummus.PDFPageModifier(pdfWriter,0);
+				/*var page = pdfWriter.createPage(0,0,595,842);
+				pdfWriter.startPageContentContext(page).writeText('Hello World',
+                                                  0,400,
+                                                  {
+                                                    font:pdfWriter.getFontForFile('C:\\Users\\Alan\\Documents\\TT-2016-B122\\fonts\\open-sans\\OpenSans-Regular.ttf'),
+                                                    size:50,
+                                                    colorspace:'gray',
+                                                    color:0x00
+                                                  });
+				pdfWriter.writePage(page);*/
+				var pageModifier = new hummus.PDFPageModifier(pdfWriter,0);
+				pageModifier.startContext().getContext().writeText(
+					day,
+					270,136,
+					{font:pdfWriter.getFontForFile('C:\\Users\\Alan\\Documents\\TT-2016-B122\\fonts\\open-sans\\OpenSans-Regular.ttf'),
+					size:15,
+					colorspace:'black',
+					color:0x00
+					}
+				);
+				pageModifier.startContext().getContext().writeText(
+					month,
+					380,136,
+					{font:pdfWriter.getFontForFile('C:\\Users\\Alan\\Documents\\TT-2016-B122\\fonts\\open-sans\\OpenSans-Regular.ttf'),
+					size:15,
+					colorspace:'black',
+					color:0x00
+					}
+				);
+				pageModifier.startContext().getContext().writeText(
+					year,
+					455,136,
+					{font:pdfWriter.getFontForFile('C:\\Users\\Alan\\Documents\\TT-2016-B122\\fonts\\open-sans\\OpenSans-Regular.ttf'),
+					size:15,
+					colorspace:'black',
+					color:0x00
+					}
+				);
+				pageModifier.endContext().writePage();
+				console.log('1');
+				pdfWriter.end();
+				console.log("fin");
+				res.redirect('/Trabajosocial/');
 	}
 	
 };
