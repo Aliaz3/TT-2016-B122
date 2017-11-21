6dc2a9c54e3612a18ff45c3fd5c1bbbd0e468da4
Aliaz3 <mk.dir35@gmail.com>
2017-11-21 02:04:54 -0600

    Parte Android

---
+++ Android/MyApplication/app/src/main/java/com/tt/alan/myapplication/AppConfig.java
@@ -0,0 +1,11 @@
+package com.tt.alan.myapplication;
+
+/**
+ * Created by Pc on 20/11/2017.
+ */
+
+public class AppConfig {
+    public static final String STREAM_URL = "rtsp://192.168.1.71:1935/live/android_test";
+    public static final String PUBLISHER_USERNAME = "Alan";
+    public static final String PUBLISHER_PASSWORD = "odmdrtal";
+}
